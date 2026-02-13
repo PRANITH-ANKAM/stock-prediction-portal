23e8c9d783622d7f404507f6788582085cda26d9
Pranith <pranith3290@gmail.com>
2026-02-13 18:31:12 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Head from './components/Head'
+import Footer from './components/Footer'
+import Main from './components/Main'
+
 
 function App() {
 
   return (
     <>
-     
+    <Head/> 
+    <Main/>
+    <Footer/>
     </>
   )
 }
